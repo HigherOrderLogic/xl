4e585026331ec42c3f6508ec810d3be4c0beebe8
HigherOrderLogic <73709188+HigherOrderLogic@users.noreply.github.com>
2026-08-29 08:24:07 +0000

    Refactor examples to make it more intuitive

--- masonry/examples/gallery/horizontal_color_picker.rs
+++ masonry/examples/gallery/horizontal_color_picker.rs
@@ -1,19 +1,19 @@
 // Copyright 2026 the Xilem Authors and the Druid Authors
 // SPDX-License-Identifier: Apache-2.0
 
-use std::cell::Cell;
-use std::rc::Rc;
+use std::mem;
 
+use accesskit::Role;
 use masonry::core::{
-    AccessCtx, ActionCtx, ChildrenIds, ErasedAction, LayoutCtx, MeasureCtx, PaintCtx,
-    PropertiesMut, PropertiesRef, RegisterCtx, Widget, WidgetId, WidgetMut, WidgetPod,
+    AccessCtx, ActionCtx, ChildrenIds, CollectionWidget, ErasedAction, LayoutCtx, MeasureCtx,
+    PaintCtx, PropertiesMut, PropertiesRef, RegisterCtx, Widget, WidgetId, WidgetMut, WidgetPod,
 };
 use masonry::imaging::Painter;
-use masonry::kurbo::{Axis, Point, Size};
+use masonry::kurbo::{Axis, Point, Rect, Size};
 use masonry::layout::{AsUnit, LayoutSize, LenReq, Length, SizeDef};
 use masonry::peniko::Color;
-use masonry::properties::{Background, TrackColor, types::CrossAxisAlignment};
-use masonry::widgets::{Flex, SizedBox, Slider, SliderMoved};
+use masonry::properties::{TrackColor, types::CrossAxisAlignment};
+use masonry::widgets::{Flex, Slider, SliderMoved};
 
 use crate::demo::CONTENT_GAP;
 
@@ -61,65 +61,140 @@ pub(crate) struct ColorSelected {
     pub color: Color,
 }
 
+#[derive(PartialEq, Debug)]
+struct ColorSliderMoved {
+    component: Component,
+    value: f64,
+}
+
+struct ColorSlider {
+    component: Component,
+    slider: WidgetPod<Slider>,
+}
+
+impl ColorSlider {
+    fn new(component: Component, value: f64) -> Self {
+        Self {
+            component,
+            slider: Slider::new(0., 1., value)
+                .prepare()
+                .with_props(TrackColor {
+                    active: component.visual_color(),
+                    ..Default::default()
+                })
+                .to_pod(),
+        }
+    }
+
+    fn set_value(this: &mut WidgetMut<'_, Self>, value: f64) {
+        let mut slider = this.ctx.get_mut(&mut this.widget.slider);
+        Slider::set_value(&mut slider, value);
+    }
+}
+
+impl Widget for ColorSlider {
+    type Action = ColorSliderMoved;
+
+    fn register_children(&mut self, ctx: &mut RegisterCtx<'_>) {
+        ctx.register_child(&mut self.slider);
+    }
+
+    fn measure(
+        &mut self,
+        ctx: &mut MeasureCtx<'_>,
+        _props: &PropertiesRef<'_>,
+        axis: Axis,
+        _len_req: LenReq,
+        cross_length: Option<Length>,
+    ) -> Length {
+        ctx.redirect_measurement(&mut self.slider, axis, cross_length)
+    }
+
+    fn on_action(
+        &mut self,
+        ctx: &mut ActionCtx<'_>,
+        _props: &mut PropertiesMut<'_>,
+        action: &ErasedAction,
+        _source: WidgetId,
+    ) {
+        if let Some(SliderMoved { value }) = action.downcast_ref::<SliderMoved>() {
+            ctx.submit_action::<Self::Action>(ColorSliderMoved {
+                component: self.component,
+                value: *value,
+            });
+            ctx.set_handled();
+        }
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx<'_>, _props: &PropertiesRef<'_>, size: Size) {
+        let size = ctx.compute_size(&mut self.slider, SizeDef::fit(size), size.into());
+        ctx.run_layout(&mut self.slider, size);
+        ctx.place_child(&mut self.slider, Point::ORIGIN);
+        ctx.derive_baselines(&self.slider);
+    }
+
+    fn paint(
+        &mut self,
+        _ctx: &mut PaintCtx<'_>,
+        _props: &PropertiesRef<'_>,
+        _painter: &mut Painter<'_>,
+    ) {
+    }
+
+    fn accessibility_role(&self) -> Role {
+        Role::GenericContainer
+    }
+
+    fn accessibility(
+        &mut self,
+        _ctx: &mut AccessCtx<'_>,
+        _props: &PropertiesRef<'_>,
+        _node: &mut accesskit::Node,
+    ) {
+    }
+
+    fn children_ids(&self) -> ChildrenIds {
+        ChildrenIds::from_slice(&[self.slider.id()])
+    }
+}
+
 pub(crate) struct HorizontalColorPicker {
     color: Color,
     widget: WidgetPod<Flex>,
-    sliders: [(Component, Rc<Cell<Option<WidgetId>>>); 4],
-    preview_id: Rc<Cell<Option<WidgetId>>>,
 }
 
 impl HorizontalColorPicker {
     pub(crate) fn new(color: Color) -> Self {
         let mut body = Flex::row().cross_axis_alignment(CrossAxisAlignment::Stretch);
 
-        let sliders = Component::ALL.map(|component| {
-            let id = Cell::new(None).into();
-            let id_target = Rc::clone(&id);
-            let widget = Slider::new(0., 1., component.get(&color) as f64)
-                .prepare()
-                .with_props(TrackColor {
-                    active: component.visual_color(),
-                    ..Default::default()
-                })
-                .with_id_callback(move |id| id_target.set(Some(id)));
-            body = std::mem::replace(&mut body, Flex::row())
+        for c in Component::ALL {
+            let widget = ColorSlider::new(c, c.get(&color) as f64).prepare();
+            body = mem::replace(&mut body, Flex::row())
                 .with(widget, 1.)
                 .with_fixed_spacer((CONTENT_GAP.get() / 2.0).px());
-            (component, id)
-        });
+        }
 
-        let preview_id = Cell::new(None).into();
-        let preview_id_target = Rc::clone(&preview_id);
-        let preview = SizedBox::empty()
-            .width(Length::const_px(20.0))
-            .prepare()
-            .with_props(Background::Color(color))
-            .with_id_callback(move |id| preview_id_target.set(Some(id)));
-        body = body.with_fixed(preview);
+        body = body.with_fixed_spacer(20.0.px());
 
         Self {
             color,
             widget: body.prepare().to_pod(),
-            sliders,
-            preview_id,
         }
     }
 
     #[expect(dead_code, reason = "Currently unused, available for future use.")]
     pub(crate) fn set_color(this: &mut WidgetMut<'_, Self>, color: Color) {
-        for (component, wid) in this.widget.sliders.iter() {
-            let value = component.get(&color);
-            this.ctx
-                .mutate_later(wid.get().unwrap(), move |mut widget| {
-                    if let Some(mut slider_widget) = widget.try_downcast::<Slider>() {
-                        Slider::set_value(&mut slider_widget, value as f64);
-                    }
-                });
+        {
+            let mut body = this.ctx.get_mut(&mut this.widget.widget);
+            for (index, component) in Component::ALL.into_iter().enumerate() {
+                let value = component.get(&color);
+                let mut slider = Flex::get_mut(&mut body, index * 2);
+                let mut slider = slider.downcast::<ColorSlider>();
+                ColorSlider::set_value(&mut slider, value as f64);
+            }
         }
-        this.ctx
-            .mutate_later(this.widget.preview_id.get().unwrap(), move |mut widget| {
-                widget.insert_prop(Background::Color(color));
-            });
+        this.widget.color = color;
+        this.ctx.request_paint_only();
     }
 }
 
@@ -154,14 +229,10 @@ impl Widget for HorizontalColorPicker {
         ctx: &mut ActionCtx<'_>,
         _props: &mut PropertiesMut<'_>,
         action: &ErasedAction,
-        source: WidgetId,
+        _source: WidgetId,
     ) {
-        if let Some(SliderMoved { value }) = action.downcast_ref::<SliderMoved>()
-            && let Some(component) = self
-                .sliders
-                .iter()
-                .find(|(_, wid)| Some(source) == wid.get())
-                .map(|(comp, _)| *comp)
+        if let Some(ColorSliderMoved { component, value }) =
+            action.downcast_ref::<ColorSliderMoved>()
         {
             #[allow(
                 clippy::cast_possible_truncation,
@@ -169,10 +240,7 @@ impl Widget for HorizontalColorPicker {
             )]
             component.update(&mut self.color, *value as f32);
             ctx.submit_action::<Self::Action>(ColorSelected { color: self.color });
-            let color = self.color;
-            ctx.mutate_later(self.preview_id.get().unwrap(), move |mut widget| {
-                widget.insert_prop(Background::Color(color));
-            });
+            ctx.request_paint_only();
             ctx.set_handled();
         }
     }
@@ -186,14 +254,24 @@ impl Widget for HorizontalColorPicker {
 
     fn paint(
         &mut self,
-        _ctx: &mut PaintCtx<'_>,
+        ctx: &mut PaintCtx<'_>,
         _props: &PropertiesRef<'_>,
-        _painter: &mut Painter<'_>,
+        painter: &mut Painter<'_>,
     ) {
+        let content_box = ctx.content_box();
+        painter.fill_rect(
+            Rect::new(
+                content_box.x1 - 20.0,
+                content_box.y0,
+                content_box.x1,
+                content_box.y1,
+            ),
+            self.color,
+        );
     }
 
-    fn accessibility_role(&self) -> accesskit::Role {
-        accesskit::Role::GenericContainer
+    fn accessibility_role(&self) -> Role {
+        Role::GenericContainer
     }
 
     fn accessibility(
